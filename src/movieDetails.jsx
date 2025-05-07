2ee4bea0e3a3eb79db60fcc94d6dab0b02384152
muskan-mujawar <muskanmujawar2505@gmail.com>
2025-05-07 23:55:56 +0530

    blur effwct added

--- src/movieDetails.jsx
+++ src/movieDetails.jsx
@@ -20,36 +20,44 @@ export default function MovieDetails() {
   }, []);
 
   return (
-    <div className="center p-4 m-4">
-      <div className="p-4 ">
-        <div className="d-flex justify-content-between">
-          <p className="d-flex  px-4 text_color">
-            <p className="pe-2">IMDB ⭐</p>
-            {info.imdbRating}
-          </p>
-          <p className="text_color ">{info.Released}</p>
-        </div>
+    <div className="centerDiv ">
+      <div
+        style={{
+          backgroundImage: `url(${info.Poster})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          borderRadius: "25px",
+        }}
+      >
+        <div className="center backColor border">
+          <div className="p-4 ">
+            <div className="d-flex justify-content-between">
+              <p className="d-flex   text_color">
+                <p>IMDB ⭐</p>
+                {info.imdbRating}
+              </p>
+              <p className="text_color ">{info.Released}</p>
+            </div>
 
-        <div></div>
+            <div></div>
 
-        <div className="d-flex">
-          <img
-            className="pe-4 pb-4 ps-4 poster"
-            src={info.Poster}
-            alt="movie poster"
-          />
-          <div>
-            <p className="text_size noto-sans-title">{info.Title}</p>
-            <p className="genre text_color pe-2 ps-2">{info.Genre}</p>
-            <p className="text_color ">⌛{info.Runtime}</p>
+            <div className="d-flex">
+              <img className=" poster" src={info.Poster} alt="movie poster" />
+              <div className="ps-4">
+                <p className="text_size grenze-gotisch-title">{info.Title}</p>
+                <p className="genre text_color pe-2 ps-2">{info.Genre}</p>
+                <p className="text_color ">⌛{info.Runtime}</p>
+              </div>
+            </div>
+            <div style={{ color: "white" }} className="jost-plot">
+              {info.Plot}
+            </div>
+            <div style={{ color: "white" }} className="pt-4 d-flex jost-plot">
+              <p className="pe-2 ">Cast : </p>
+              {info.Actors}
+            </div>
           </div>
-        </div>
-        <div style={{ color: "white" }} className="noto-sans-plot">
-          {info.Plot}
-        </div>
-        <div style={{ color: "white" }} className="pt-4 d-flex noto-sans-plot">
-          <p className="pe-2 ">Cast : </p>
-          {info.Actors}
         </div>
       </div>
     </div>
