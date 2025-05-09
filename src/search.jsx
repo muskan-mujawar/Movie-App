379196efe6c97f1788ad60fc92bfe3b5565a1e21
muskan-mujawar <muskanmujawar2505@gmail.com>
2025-05-09 20:40:39 +0530

    added shadow

--- src/search.jsx
+++ src/search.jsx
@@ -35,34 +35,37 @@ export default function Search() {
         </h2>
       </div>
       <div className="center ">
-        <div className="input-group mb-3 design  ">
-          <input
-            type="text"
-            className=" box input ps-2 grad "
-            placeholder="Search your movies.. "
-            value={inputValue}
-            onChange={(e) => setInputvalue(e.target.value)}
-          />
-          <button
-            className="btn  button grad "
-            onClick={() => {
-              fetchData();
-              handleClick();
-            }}
-            type="button"
-            id="button-addon2"
-            style={{ backgroundColor: "white" }}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="1em"
-              height="1em"
-              viewBox="0 0 50 50"
-              style={{ borderRadius: "25px" }}
+        <div className="grad ">
+          <div className="input-group  design ">
+            <input
+              type="text"
+              className=" box input ps-2  "
+              placeholder="Search your movies.. "
+              value={inputValue}
+              onChange={(e) => setInputvalue(e.target.value)}
+              style={{ color: "white" }}
+            />
+            <div
+              className="btn  button  "
+              onClick={() => {
+                fetchData();
+                handleClick();
+              }}
             >
-              <path d="M 21 3 C 11.621094 3 4 10.621094 4 20 C 4 29.378906 11.621094 37 21 37 C 24.710938 37 28.140625 35.804688 30.9375 33.78125 L 44.09375 46.90625 L 46.90625 44.09375 L 33.90625 31.0625 C 36.460938 28.085938 38 24.222656 38 20 C 38 10.621094 30.378906 3 21 3 Z M 21 5 C 29.296875 5 36 11.703125 36 20 C 36 28.296875 29.296875 35 21 35 C 12.703125 35 6 28.296875 6 20 C 6 11.703125 12.703125 5 21 5 Z"></path>
-            </svg>
-          </button>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="1em"
+                height="1em"
+                viewBox="0 0 50 50"
+                style={{ borderRadius: "25px" }}
+              >
+                <path
+                  fill="white"
+                  d="M 21 3 C 11.621094 3 4 10.621094 4 20 C 4 29.378906 11.621094 37 21 37 C 24.710938 37 28.140625 35.804688 30.9375 33.78125 L 44.09375 46.90625 L 46.90625 44.09375 L 33.90625 31.0625 C 36.460938 28.085938 38 24.222656 38 20 C 38 10.621094 30.378906 3 21 3 Z M 21 5 C 29.296875 5 36 11.703125 36 20 C 36 28.296875 29.296875 35 21 35 C 12.703125 35 6 28.296875 6 20 C 6 11.703125 12.703125 5 21 5 Z"
+                ></path>
+              </svg>
+            </div>
+          </div>
         </div>
       </div>
       {item.length == 0 && (
